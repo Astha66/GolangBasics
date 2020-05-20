952ee0c1678bf0598156777f52e53c0c5ca5e9b4
Astha Sharma <Astha.Sharma@iiitb.org>
2020-05-20 11:45:44 +0530

    Update Variable_scoping.go

--- test/Variable_scoping.go
+++ test/Variable_scoping.go
@@ -6,7 +6,7 @@ var a=9
 func demo(){
 	a=8
 	fmt.Println("In demo: ",a) //function gives preference to its own variable first- function level variable
-}
+}	// this is called shadowing
 func main(){
 	demo()
 	fmt.Println("In main: ",a) //package level variable
